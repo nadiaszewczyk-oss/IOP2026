65fdaac56bc3800344b7dba53ce8ab306ffd38f3
nadiaszewczyk <328017@stud.umk.pl>
2026-09-25 10:44:45 +0200

    Poprawiony kod

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
 int nwd(int a, int b){
 	while(b!=0){
@@ -15,7 +15,7 @@ int main(){
 	int a, b;
 	cin>>a>>b;
 
-	if(a!<0 || b!<0){
+	if(a<0 || b<0){
 		cout<<"Liczby musza byc dodatnie"<<endl;
 		return 0;
 	}
