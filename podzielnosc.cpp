e2d1e11cd9c95fed2f4d1dfeff9f59976657f2ce
nadiaszewczyk <328017@stud.umk.pl>
2026-09-25 09:50:58 +0200

    Dodanie funkcji nwd

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std
 
+int nwd(int a, int b){
+	while(b!=0){
+	int r = a%b;
+	a=b;
+	b=r;
+	}
+	return a;
+}
+
 int main(){
 
 	int a, b;
 	cin>>a>>b;
-	cout<<a<<" "<<b<<endl;
+
+	cout<<"Największy wspolny dzielnik liczb "<<a<<" i "<<b<<": "<<nwd(a,b)<<endl;
+
+	
 
 return 0;
 }
